343928fa375abf13863c7bb54e22962b93003962
nizami111 <83838042+nizami111@users.noreply.github.com>
2021-05-11 22:07:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,8 +49,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler("Валера", "E3"))
-    #dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, echo))
     
 
     # log all errors
